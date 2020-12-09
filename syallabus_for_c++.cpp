0b4feb81750c44ffabee60a9203a674c36ed4811
vikrant0 <65070168+vikrant0@users.noreply.github.com>
2020-12-09 22:58:46 +0530

    Create syallabus_for_c++.cpp

---
+++ syallabus_for_c++.cpp
@@ -0,0 +1,251 @@
+UNIT 1
+
+
+Chapter-1: Fundamentals of C++
+
+ 
+
+1. Features of object-oriented programming
+
+​
+
+2. Difference between object oriented and procedure-oriented programming
+
+​
+
+3. Difference between structure and class
+
+​
+
+4. Data types
+
+​
+
+5. Input and output streams (cin cout)
+
+​
+
+6. Introduction to namespace​​
+
+​
+
+
+Chapter-2: Classes and Objects
+
+ 
+
+1.) Specifying a class
+
+​
+
+2.) Creating objects
+
+​
+
+3.) Accessing class members
+
+​
+
+4.) Defining a member function inside and outside class
+
+​
+
+5.) Access specifiers
+
+​
+
+6.) Inline function
+
+​
+
+7.) Static data members & member functions
+
+​
+
+8.) Objects as function arguments
+
+​
+
+9.) Friend function
+
+​
+
+10.) Returning objects to functions
+
+​
+
+
+Chapter-3: Constructors and Destructors
+
+​
+
+1.) Need for constructors
+
+​
+
+2.) Types of constructors: default parameterized copy constructor
+
+​
+
+3.) Order of execution of constructors
+
+​
+
+4.) Destructors and their need
+
+​
+
+ 
+
+ 
+
+UNIT 2
+
+
+Chapter-4: Inheritance
+
+​
+
+1.) Defining derived class
+
+​
+
+2.) Modes of inheritance
+
+​
+
+3.) Types of inheritance
+
+​
+
+4.) Ambiguity in inheritance
+
+​
+
+5.) Virtual base class
+
+​
+
+6.) Function overriding
+
+​
+
+7.) Member Classes: Nesting of Classes
+
+​
+
+
+Chapter-5: Polymorphism
+
+​
+
+1.) Introduction & types of polymorphism
+
+​
+
+2.) Function overloading
+
+​
+
+3.) Operator overloading
+
+​
+
+4.) Rules for overloading operators
+
+​
+
+5.) Overloading of unary & binary operators
+
+​
+
+6.) Constructor Overloading
+
+ 
+
+
+Chapter-6: Exception Handling
+
+​
+
+1.) Try Throw Catch
+
+​
+
+2.) Throwing an Exception
+
+​
+
+3.) Catching an Exception
+
+​
+
+​​
+
+UNIT 3
+
+​
+
+Chapter-7: Pointers, Virtual Functions
+
+​
+
+1.) Declaring & initializing pointers
+
+​
+
+2.) Pointer to objects
+
+​
+
+3.) This pointer
+
+​
+
+4.) Pointer to derived classes
+
+​
+
+5.) Static and dynamic binding
+
+​
+
+
+Chapter-8: Dynamic memory allocation
+
+​
+
+1.) Dynamic memory allocation using new and delete operator
+
+​
+
+
+Chapter-9: Files
+
+​
+
+1.) Introduction to File streams
+
+​
+
+2.) Hierarchy of file stream classes
+
+​
+
+3.) File operations
+
+​
+
+4.) File I/O
+
+​
+
+5.) File opening Modes
+
+​
+
+6.) Reading/Writing of files
+
+​
+
+7.) Random-access to files
